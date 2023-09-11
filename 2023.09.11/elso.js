42fe397d46026980b03b371197beea5d10303cb8
EvelinHollik2 <hollikevi@gmail.com>
2023-09-11 08:58:53 +0200

    js 1

---
+++ 2023.09.11/elso.js
@@ -0,0 +1,27 @@
+const a_input = document.getElementById("a_szam");
+const b_input = document.querySelector("#b_szam");
+const kijelzo = document.querySelector("#eredmeny");
+
+function osszeadd() {
+    a=parseFloat (a_input.value);
+    b=parseFloat (b_input.value);
+    kijelzo.innerHTML = "összeadva: " + (a+b) .toString();
+}
+
+function kivon(){
+    a=parseFloat (a_input.value);
+    b=parseFloat (b_input.value);
+    kijelzo.innerHTML = "kivonva: " + (a-b) .toString();
+}
+
+function szoroz(){
+    a=parseFloat (a_input.value);
+    b=parseFloat (b_input.value);
+    kijelzo.innerHTML = "szorozva: " + (a*b) .toString();
+}
+
+function oszt(){
+    a=parseFloat (a_input.value);
+    b=parseFloat (b_input.value);
+    kijelzo.innerHTML = "osztva: " + (a/b) .toString();
+}
